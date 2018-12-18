b9641b78ca5cd504565835e3b67852611b316ea4
Savannah Fox <savannahfox@gmail.com>
2018-12-18 10:19:09 -0800

    Initial commit of BooleanLogicProject.

---
+++ BooleanLogicProject/BooleanLogicProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BooleanLogicProject
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("What is your age?");
+            string answerAge = Console.ReadLine();
+            int applicantAge = Convert.ToInt32(answerAge);
+            bool isOldEnough = applicantAge > 15;
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string answerDUI = Console.ReadLine();
+            bool applicantDUI = Convert.ToBoolean(answerDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string answerTickets = Console.ReadLine();
+            int numberTickets = Convert.ToInt32(answerTickets);
+            bool applicantTickets = numberTickets < 3;
+
+            bool isEligible = (isOldEnough == true && applicantDUI == false && applicantTickets == true);
+            Console.WriteLine(isEligible);
+
+            Console.ReadLine();
+
+        }
+    }
+}
