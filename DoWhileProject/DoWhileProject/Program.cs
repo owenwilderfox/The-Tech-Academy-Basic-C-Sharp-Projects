cd307bb5c487672bfe1b79b61bad4055ffd22453
Savannah Fox <savannahfox@gmail.com>
2018-12-18 12:16:07 -0800

    Initial commit of project.

---
+++ DoWhileProject/DoWhileProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("I am thinking of an animal. Can you guess what I am thinking of?");
+            string animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case "cat":
+                    Console.WriteLine("You guessed a cat. I'm sorry, that's incorrect.");
+                    break;
+                case "elephant":
+                    Console.WriteLine("You guessed an elephant. I'm sorry, that's incorrect.");
+                    break;
+                case "zebra":
+                    Console.WriteLine("You guessed a zebra. Good job! You're great at guessing.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
