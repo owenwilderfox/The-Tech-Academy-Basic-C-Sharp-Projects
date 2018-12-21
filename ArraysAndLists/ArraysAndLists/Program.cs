5b1c69929dc2f8a05616c7ce0ecda80f37697dd6
Savannah Fox <savannahfox@gmail.com>
2018-12-21 10:55:55 -0800

    Removal of extraneous code.

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -46,30 +46,6 @@ namespace ArraysAndLists
             Console.WriteLine("You have selected index " + listNumber + ". The cat name at that index is " + intList[listNumber] + ".");
 
             Console.ReadLine();
-
-
-
-            //int[] numArray = new int[5];
-            //numArray[0] = 5;
-            //numArray[1] = 2;
-            //numArray[2] = 10;
-            //numArray[3] = 200;
-            //numArray[4] = 5000;
-
-            //int[] numArray1 = new int[] { 5, 2, 10, 200, 500 };
-            //int[] numArray2 = { 1, 2, 3, 200, 5400, 212 };
-
-            //numArray2[5] = 650;
-
-            //Console.WriteLine(numArray2[5]);
-            //Console.ReadLine();
-
-            //List<string> intList = new List<string>();
-            //intList.Add("Hello");
-            //intList.Add("Fred");
-
-            //Console.WriteLine(intList[1]);
-            //Console.ReadLine();
         }
     }
 }
