42e57864984de80f7fffbddf8a4d084ec89249d2
Savannah Fox <savannahfox@gmail.com>
2018-12-21 13:33:43 -0800

    Add solutions #2 and #3.

--- Iteration/Iteration/Program.cs
+++ Iteration/Iteration/Program.cs
@@ -10,10 +10,7 @@ namespace Iteration
 
             //#1: 1. Create a one-dimensional Array of strings. Ask the user to input some text. Create a loop that goes through each string in the Array, adding the user’s text to the string. Then create a loop that prints off each string in the Array on a separate line.
 
-            // Create array of strings.
             string[] dogNames = new string[5];
-
-            // Request information from user.
             Console.WriteLine("Let's make a list of five dog names you enjoy:");
 
             for (int i = 0; i < dogNames.Length; i++)
@@ -22,7 +19,6 @@ namespace Iteration
                 dogNames[i] = Console.ReadLine();
             }
 
-            // Print dog names entered.
             Console.WriteLine("Here are the names that you like:");
 
             for (int i = 0; i < dogNames.Length; i++)
@@ -31,6 +27,26 @@ namespace Iteration
             }
 
             //#2. Create an infinite loop.
+
+            //Console.WriteLine("Let's do some multiplication. Please enter a number:");
+            //int enteredNumber = Convert.ToInt32(Console.ReadLine());
+            //for (; ; )
+            //{
+            //    enteredNumber = enteredNumber * 9;
+            //    Console.WriteLine(enteredNumber);
+            //}
+
+            //#3. Fix the infinite loop so it will execute.
+
+            Console.WriteLine("Let's do some multipliation. Please enter a number:");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+            for (int n = 1; n < 5; n++)
+            {
+                enteredNumber = enteredNumber * 9;
+                Console.WriteLine(enteredNumber);
+            }
+
+
 
             Console.ReadLine();
 
