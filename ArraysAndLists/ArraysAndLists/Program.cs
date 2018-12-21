784361f73a94f4b440873d76779f6d5df58b6421
Savannah Fox <savannahfox@gmail.com>
2018-12-21 10:55:12 -0800

    Addition of string array and list, as well as an error message when user selects an index that does not exist.

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -7,17 +7,43 @@ namespace ArraysAndLists
     {
         static void Main()
         {
-            int[] numArray = new int[6];
-            numArray[0] = 100;
-            numArray[1] = 200;
-            numArray[2] = 300;
-            numArray[3] = 400;
-            numArray[4] = 500;
-            numArray[5] = 600;
+            // String array
+            string[] catName = { "Chief", "Loki", "Graham", "Elliott" };
+            Console.WriteLine("Please select an index from the array:");
+            int selectedCat = Convert.ToInt32(Console.ReadLine());
+            if (selectedCat > 3)
+            {
+                Console.WriteLine("That index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine("That index has a string value of " + catName[selectedCat] + ".");
+            }
 
-            Console.WriteLine("Please select an index from the pre-built array:");
+
+            // Int array
+            int[] numArray = { 100, 200, 300, 400, 500, 600 };
+            Console.WriteLine("Next, please select an index from the pre-built array:");
             int selectedNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("That index has an integer of " + numArray[selectedNumber] + ".");
+            if (selectedNumber > 5)
+            {
+                Console.WriteLine("That index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine("That index has an integer of " + numArray[selectedNumber] + ".");
+            }
+
+            // List of strings
+            List<string> intList = new List<string>();
+            intList.Add("Chief");
+            intList.Add("Loki");
+            intList.Add("Elliott");
+            intList.Add("Graham");
+
+            Console.WriteLine("Please select another index to get a cat name from the list:");
+            int listNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have selected index " + listNumber + ". The cat name at that index is " + intList[listNumber] + ".");
 
             Console.ReadLine();
 
