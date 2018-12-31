70659305ae6014c89d8ff79b89c13420ae814fc5
Savannah Fox <savannahfox@gmail.com>
2018-12-31 12:17:31 -0800

    Initial commit of exceptions project.

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] mathArray = new int[5];
+
+
+                for (int i = 0; i < mathArray.Length; i++)
+                {
+                    Console.WriteLine("Please enter an integer: ");
+                    mathArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("Please enter an integer that you would like the divide your previously entered integers by:");
+                int divideBy = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dividing numbers now...");
+
+                for (int i = 0; i < mathArray.Length; i++)
+                {
+                    int answer = mathArray[i] / divideBy;
+                    Console.WriteLine(mathArray[i] + " divided by " + divideBy + " is " + answer + ".");
+                }
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please do not divide by zero.");
+                return;
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please only enter integers.");
+                return;
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
