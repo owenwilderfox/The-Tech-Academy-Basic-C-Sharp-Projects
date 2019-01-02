7974afca763bdeec59a640b85eb0326996259e60
Savannah Fox <savannahfox@gmail.com>
2019-01-02 13:47:09 -0800

    Add note.

---
+++ ClassDrill/ClassDrill/Number.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    public class Number
+    {
+        public Number()
+        {
+            Value = 1;
+        }
+        public int Value { get; set; }
+
+        //Methods
+        public static Number Add(Number number)
+        {
+
+            int tempAddnumber = number.Value + 10;
+            number.Value = tempAddnumber;
+            return number;
+
+        }
+
+        public static Number Divide(Number number)
+        {
+
+            int tempDividenumber = number.Value / 2;
+            number.Value = tempDividenumber;
+            return number;
+
+        }
+
+        public static Number Multiply(Number number)
+        {
+            int tempMultiplynumber = number.Value * 20;
+            number.Value = tempMultiplynumber;
+            return number;
+        }
+
+    }
+}
---
+++ ClassDrill/ClassDrill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This program will take any number entered and add 10 to it, divide it by 2, and multiply it by 20.");
+            Console.WriteLine("Please enter an interger you would like to work with:");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+            Number number = new Number();
+            number.Value = enteredNumber;
+
+            //Call functions from class.
+            Number.Add(number);
+            Number.Divide(number);
+            Number.Multiply(number);
+
+            Console.WriteLine("After processing, your number is now " + number.Value + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
