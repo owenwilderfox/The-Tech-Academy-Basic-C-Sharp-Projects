062fdf806009000f7dc6ffbca34e709323ee25d4
Savannah Fox <savannahfox@gmail.com>
2019-01-03 13:52:21 -0800

    Commit working Parameter drill with public class.

--- Parameter/Parameter/Program.cs
+++ Parameter/Parameter/Program.cs
@@ -14,22 +14,14 @@ namespace Parameter
             Console.WriteLine("What number would you like to work with today?");
             int enteredNumber = Convert.ToInt32(Console.ReadLine());
 
-            //Number number = new Number();
-            //number.Value = enteredNumber;
-            //number = Number.MultiplicationLoop(number: number, times: 1);
-
             Number number = new Number();
             number.Value = enteredNumber;
             int timesMultiplied = 0;
             number = Number.MultiplicationLoop(number, out timesMultiplied, 4);
 
-            //Number.MultiplicationLoop(number);
-
-            Console.WriteLine("Your new number is: " + number.Value);
-            Console.WriteLine("Your number (" + enteredNumber + ") was multipied by two and then added together " + timesMultiplied + " times.");
+            Console.WriteLine("Your number (" + enteredNumber + ") went through a multiplication loop " + timesMultiplied + " times, multipying the product by two each time.");
+            Console.WriteLine("Your new number is " + number.Value + ".");
             Console.ReadLine();
         }
     }
 }
-
-//enteredNumber as parameter, times as parameter
