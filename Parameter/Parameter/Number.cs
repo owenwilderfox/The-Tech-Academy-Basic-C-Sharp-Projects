2b6ac90043b9258809656325903a5b765ad2e9b4
Savannah Fox <savannahfox@gmail.com>
2019-01-03 12:52:43 -0800

    Initial commit of Parameter drill.

---
+++ Parameter/Parameter/Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    class Number
+    {
+        public Number()
+        {
+            Value = 1;
+        }
+        
+        public int Value { get; set; }
+
+        //Method
+        public static Number MultiplicationLoop(Number number)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int tempNumberValue = number.Value * 5;
+                number.Value = tempNumberValue;
+                i++;
+            }
+            return number;
+        }
+    }
+}
---
+++ Parameter/Parameter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This program will take two numbers and multiply them together several times.");
+            Console.WriteLine("What number would you like to work with today?");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+            Number number = new Number();
+            number.Value = enteredNumber;
+
+            Number.MultiplicationLoop(number);
+
+            Console.WriteLine("Your new number is: " + number.Value);
+            Console.WriteLine("Your number (" + enteredNumber + ") was multipied " + times + " times.");
+            Console.ReadLine();
+        }
+    }
+}
