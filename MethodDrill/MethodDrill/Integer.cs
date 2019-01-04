bc2038bf777ae6bd37ed8cf5cfbfd751b96df5bf
Savannah Fox <savannahfox@gmail.com>
2019-01-04 09:45:24 -0800

    Add second method.

--- MethodDrill/MethodDrill/Integer.cs
+++ MethodDrill/MethodDrill/Integer.cs
@@ -14,5 +14,22 @@ namespace MethodDrill
         }
 
         public int Value { get; set; }
+        public decimal decValue { get; set; }
+
+        //Method One
+        public static Integer Add(Integer firstInteger)
+        {
+            int tempFirstInteger = firstInteger.Value + 55;
+            firstInteger.Value = tempFirstInteger;
+            return firstInteger;
+        }
+
+        //Method Two
+        public static Integer Multiply(Integer secondInteger)
+        {
+            decimal tempSecondIntger = secondInteger.decValue * 2;
+            secondInteger.decValue = tempSecondIntger;
+            return secondInteger;
+        }
     }
 }
--- MethodDrill/MethodDrill/Program.cs
+++ MethodDrill/MethodDrill/Program.cs
@@ -11,7 +11,29 @@ namespace MethodDrill
         static void Main(string[] args)
         {
 
-            Console.WriteLine("")
+            Console.WriteLine("This program will do math operations on entered numbers.");
+
+            //First Integer
+            Console.WriteLine("Please enter the first whole number that you would like to work with:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Integer firstInteger = new Integer();
+            firstInteger.Value = firstNumber;
+            Integer.Add(firstInteger);
+
+            Console.WriteLine("Your number - " + firstNumber + " - was added to 55. Your number is now " + firstInteger.Value + ".");
+
+            //Second Integer
+            Console.WriteLine("Let's use a different number. You may enter a decimal here:");
+            decimal secondNumber = Convert.ToDecimal(Console.ReadLine());
+
+            Integer secondInteger = new Integer();
+            secondInteger.decValue = secondNumber;
+            Integer.Multiply(secondInteger);
+
+            Console.WriteLine("Your second number - " + secondNumber + " - was multiplied by 2. Your number is now " + secondInteger.decValue + ".");
+
+            Console.ReadLine();
 
         }
     }
