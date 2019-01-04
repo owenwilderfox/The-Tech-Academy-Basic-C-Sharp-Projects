7efdbcd638c69d22e23abb4b9fd35896962161de
Savannah Fox <savannahfox@gmail.com>
2019-01-04 14:09:53 -0800

    Commit changes to program.cs and deck.cs.

--- ClassesandObjects/ClassesandObjects/Deck.cs
+++ ClassesandObjects/ClassesandObjects/Deck.cs
@@ -32,5 +32,22 @@ namespace TwentyOne
         }
 
         public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+
+                Cards = TempList;
+            }
+        }
     }
 }
