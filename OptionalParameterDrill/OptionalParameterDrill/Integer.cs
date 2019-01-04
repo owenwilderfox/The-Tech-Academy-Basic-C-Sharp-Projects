15ba90bdc9ab38dbfba588612ba706ca6fccc212
Savannah Fox <savannahfox@gmail.com>
2019-01-04 13:55:03 -0800

    Commit changes to Parameter Drill.

---
+++ OptionalParameterDrill/OptionalParameterDrill/Integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterDrill
+{
+    class Integer
+    {
+        public int Add(int firstInteger, int optionalInteger = 56)
+        {
+            int result = firstInteger + optionalInteger;
+            return result;
+        }
+    }
+}
---
+++ OptionalParameterDrill/OptionalParameterDrill/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Please enter a number:");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Optional: please enter another number:");
+                string tempNumber = Console.ReadLine();
+
+                if(tempNumber == "")
+                {
+                    Integer firstInteger = new Integer();
+                    int result = firstInteger.Add(firstNumber);
+                    Console.WriteLine("Added default number. Answer: " + result);
+                }
+
+                else
+                {
+
+                    int optionalNumber = Convert.ToInt32(tempNumber);
+                    Integer firstInteger = new Integer();
+                    int result = firstInteger.Add(firstNumber, optionalNumber);
+                    Console.WriteLine("Answer: " + result);
+                }
+
+                Console.ReadLine();
+            
+        }
+    }
+}
