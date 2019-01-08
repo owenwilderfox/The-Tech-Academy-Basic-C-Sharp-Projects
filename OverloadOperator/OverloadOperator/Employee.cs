8d9271392f7d15fd30b7050ae03188a033132a74
Savannah Fox <savannahfox@gmail.com>
2019-01-08 15:00:33 -0800

    Commit changes to OverloadOperator.

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        public override void SayName()
+        {
+            base.SayName();
+            Console.WriteLine("ID: {0}", Id);
+        }
+
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+                return true;
+            else
+                return false;
+        }
+        
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 100
+            };
+
+            Employee employee2 = new Employee()
+            {
+                FirstName = "S.",
+                LastName = "Student",
+                Id = 100
+            };
+
+            employee1.SayName();
+            employee2.SayName();
+
+            if (employee1 == employee2)
+                Console.WriteLine("These employees are equal.");
+            else
+                Console.WriteLine("These employees are not equal.");
+
+            Console.ReadLine();
+        }
+    }
+}
