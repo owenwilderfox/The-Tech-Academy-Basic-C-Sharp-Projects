7d15b8bb6c41ff55f204ab59cdeef1e94764966a
Savannah Fox <savannahfox@gmail.com>
2019-01-08 10:14:25 -0800

    Initial commit of Class Drill 2 with Methods 1 and 2.

---
+++ ClassDrill2/ClassDrill2/Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Number
+    {
+
+        public void Division(int enteredNumber)
+        {
+            int quotient = enteredNumber / 2;
+            Console.WriteLine("Answer: {0}", quotient);
+            return;
+        }
+
+        public int Multiplication(int enteredNumber2, out int times)
+        {
+            times = 5;
+            int product = enteredNumber2 * times;
+            return product;
+        }
+
+
+    }
+}
---
+++ ClassDrill2/ClassDrill2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////First Method
+            //Console.WriteLine("Please enter a number to divide in half:");
+            //int integer = Convert.ToInt32(Console.ReadLine());
+
+            //Number enteredNumber = new Number();
+            //enteredNumber.Division(integer);
+
+            ////Second Method
+            //Console.WriteLine("Enter another number to work with:");
+            //int integer2 = Convert.ToInt32(Console.ReadLine());
+
+            //Number enteredNumber2 = new Number();
+            //int result2 = enteredNumber2.Multiplication(integer2, out int times);
+
+            //Console.WriteLine("Answer: {0}", result2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
