1fe12a6597e7bd84dcb64281abdd3aee8d8357c6
Savannah Fox <savannahfox@gmail.com>
2019-01-08 12:20:47 -0800

    Commit AbstractClass drill.

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            person.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
