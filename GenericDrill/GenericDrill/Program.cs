9d918e77a753559ac49aa75184b4582748cf548f
Savannah Fox <savannahfox@gmail.com>
2019-01-10 13:09:44 -0800

    Upload generic drill.

---
+++ GenericDrill/GenericDrill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Employees = new Employee<string>() { "John", "Fred" };
+
+            Employee<int> EmployeeID = new Employee<int>() { 1, 2, 3 };
+        }
+    }
+}
