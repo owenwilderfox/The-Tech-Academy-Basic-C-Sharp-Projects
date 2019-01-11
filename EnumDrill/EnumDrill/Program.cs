bfb8e9dd80de4e473022b5c94b113cf09007407d
Savannah Fox <savannahfox@gmail.com>
2019-01-11 10:44:27 -0800

    Initial commit of EnumDrill.

---
+++ EnumDrill/EnumDrill/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+                string value = Console.ReadLine();
+
+                if (Enum.IsDefined(typeof(DaysOfTheWeek), value))
+                {
+                    Console.WriteLine("{0} is a day of the week.", value);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday=2,
+            Tuesday=3,
+            Wednesday=4,
+            Thursday=5,
+            Friday=6,
+            Saturday=7,
+            Sunday=1
+        }
+    }
+}
