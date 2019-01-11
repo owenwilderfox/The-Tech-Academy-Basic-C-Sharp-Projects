2ba9b2e1d86d9be1e932d2d01578c7526554ae61
Savannah Fox <savannahfox@gmail.com>
2019-01-11 09:21:15 -0800

    Commit changes to drill.

--- GenericDrill/GenericDrill/Program.cs
+++ GenericDrill/GenericDrill/Program.cs
@@ -10,15 +10,21 @@ namespace GenericDrill
     {
         static void Main(string[] args)
         {
-            Employee<string> Employees = new Employee<string>() { Things = { "John", "Fred", "Mark" } };
+            Employee<string> Employees = new Employee<string>() { Things = new List<string> { "John", "Fred", "Mark" } };
 
-            Employee<int> EmployeeID = new Employee<int>() { Things = { 1, 2, 3 } };
+            Employee<int> EmployeeID = new Employee<int>() { Things = new List<int> { 1, 2, 3 } };
 
-            foreach (string employee in Employees)
+            foreach (string Employee in Employees.Things)
             {
-
+                Console.WriteLine(Employee);
             }
 
+            foreach (int ID in EmployeeID.Things)
+            {
+                Console.WriteLine(ID);
+            }
+
+            Console.ReadLine();
         }
     }
 }
