c233a203800f43f92af34c3efd68fab5c24ba418
Savannah Fox <savannahfox@gmail.com>
2019-01-14 12:03:19 -0800

    Add working solution for third problem; clean up code and notes.

--- LambdaExpressionDrill/LambdaExpressionDrill/Program.cs
+++ LambdaExpressionDrill/LambdaExpressionDrill/Program.cs
@@ -11,45 +11,46 @@ namespace LambdaExpressionDrill
         static void Main(string[] args)
         {
             List<Employee> employeeList= new List<Employee>();
-            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Smith", ID = 100 });
-            employeeList.Add(new Employee { FirstName = "Elizabeth", LastName = "Anderson", ID = 101 });
-            employeeList.Add(new Employee { FirstName = "Kelly Anne", LastName = "Franklin", ID = 102 });
-            employeeList.Add(new Employee { FirstName = "Elliott", LastName = "McCall", ID = 103 });
-            employeeList.Add(new Employee { FirstName = "Graham", LastName = "Fox", ID = 104 });
-            employeeList.Add(new Employee { FirstName = "Fuller", LastName = "Bryant", ID = 105 });
-            employeeList.Add(new Employee { FirstName = "Grant", LastName = "Colton", ID = 106 });
-            employeeList.Add(new Employee { FirstName = "Emily", LastName = "Rainier", ID = 107 });
-            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Helens", ID = 108 });
-            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Everett", ID = 109 });
+            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Smith", ID = 1 });
+            employeeList.Add(new Employee { FirstName = "Elizabeth", LastName = "Anderson", ID = 2 });
+            employeeList.Add(new Employee { FirstName = "Kelly Anne", LastName = "Franklin", ID = 3 });
+            employeeList.Add(new Employee { FirstName = "Elliott", LastName = "McCall", ID = 4 });
+            employeeList.Add(new Employee { FirstName = "Graham", LastName = "Fox", ID = 5 });
+            employeeList.Add(new Employee { FirstName = "Fuller", LastName = "Bryant", ID = 6 });
+            employeeList.Add(new Employee { FirstName = "Grant", LastName = "Colton", ID = 7 });
+            employeeList.Add(new Employee { FirstName = "Emily", LastName = "Rainier", ID = 8 });
+            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Helens", ID = 9 });
+            employeeList.Add(new Employee { FirstName = "Joe", LastName = "Everett", ID = 10 });
 
-            //// Working solution for #2: create a foreach loop that singles out everyone with the first name "Joe" and add to a new list
-            //List<Employee> joeList = new List<Employee>();
+            // Working solution for #2: create a foreach loop that singles out everyone with the first name "Joe" and add to a new list
+            List<Employee> joeList = new List<Employee>();
 
-            //foreach (var employee in employeeList)
-            //{
-            //    if (employee.FirstName.Contains("Joe"))
-            //    {
-            //        joeList.Add(employee);
-            //    }
-            //}
+            foreach (var employee in employeeList)
+            {
+                if (employee.FirstName.Contains("Joe"))
+                {
+                    joeList.Add(employee);
+                }
+            }
 
-            //foreach(var joe in joeList)
-            //{
-            //    Console.WriteLine("{0} {1} {2}", joe.FirstName, joe.LastName, joe.ID);
-            //}
+            foreach (var joe in joeList)
+            {
+                Console.WriteLine("{0} {1} {2}", joe.FirstName, joe.LastName, joe.ID);
+            }
 
+            // Working solution for #3: create a separate list for employees with the first name "Joe" with a Lambda expression
+            List<Employee> joeList2 = employeeList.Where(x => x.FirstName.Contains("Joe")).ToList();
+            foreach (Employee joe in joeList2)
+            {
+                Console.WriteLine("{0} {1} {2}", joe.FirstName, joe.LastName, joe.ID);
+            }
 
-            //Console.WriteLine("{0} {1} {2}", employee.FirstName, employee.LastName, employee.ID);
-
-
-            //// Working solution for #3: create separate list with a Lambda expression
-            //List<Employee> joeList2 = employeeList.Where(x => x.FirstName.Contains("Joe")).ToList();
-            //foreach (Employee joe in joeList2)
-            //{
-            //    Console.WriteLine("{0} {1} {2}", joe.FirstName, joe.LastName, joe.ID);
-            //}
-
-            //
+            // Working solution for #4: create a seperate list for employees with an ID greater than 5 with a Lambda expression
+            List<Employee> joeList3 = employeeList.Where(x => x.ID > 5).ToList();
+            foreach (Employee joe in joeList3)
+            {
+                Console.WriteLine("{0} {1} {2}", joe.FirstName, joe.LastName, joe.ID);
+            }
 
             Console.ReadLine();
         }
