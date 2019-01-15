050b9236bb6ac3217db5de02dd5ab893f193c86b
Savannah Fox <savannahfox@gmail.com>
2019-01-15 10:10:06 -0800

    Commit changes to game.

--- ClassesandObjects/ClassesandObjects/TwentyOneGame.cs
+++ ClassesandObjects/ClassesandObjects/TwentyOneGame.cs
@@ -8,9 +8,64 @@ namespace TwentyOne
 {
     public class TwentyOneGame : Game, IWalkAway
     {
+        public TwentyOneDealer Dealer { get; set; }
+
         public override void Play()
         {
-            throw new NotImplementedException();
+            Dealer = new TwentyOneDealer();
+            foreach (Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            Console.WriteLine("Place your bet:");
+
+            foreach(Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+                bool successfullyBet = player.Bet(bet);
+                if (!successfullyBet)
+                {
+                    return;
+                }
+                Bets[player] = bet;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Dealing...");
+                foreach (Player player in Players)
+                {
+                    Console.Write("{0}: ", player.Name);
+                    Dealer.Deal(player.Hand);
+                    if (i == 1)
+                    {
+                        bool blackJack = TwentyOneRules.CheckForBlackJack(player.Hand);
+                        if (blackJack)
+                        {
+                            Console.WriteLine("BlackJack! {0} wins {1}!", player.Name, Bets[player]);
+                            player.Balance += Convert.ToInt32((Bets[player] * 1.5) + Bets[player]);
+                            return;
+                        }
+                    }
+                }
+                Console.Write("Dealer: ");
+                Dealer.Deal(Dealer.Hand);
+                if (i == 1)
+                {
+                    bool blackJack = TwentyOneRules.CheckForBlackJack(Dealer.Hand);
+                    if (blackJack)
+                    {
+                        Console.WriteLine("Dealer has BlackJack! Everyone lose.");
+                        foreach (KeyValuePair<Player, int> entry in Bets)
+                        {
+                            Dealer.Balance += entry.Value;
+                        }
+                    }
+                }
+            }
         }
 
         public override void ListPlayers()
