b5dc570f31475f594d4cb05eab07322d583aa57c
Savannah Fox <savannahfox@gmail.com>
2019-01-15 13:13:46 -0800

    Initial commit of DateTime drill.

--- ClassesandObjects/ClassesandObjects/Dealer.cs
+++ ClassesandObjects/ClassesandObjects/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\My DELL\logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
                 Deck.Cards.RemoveAt(0);
---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateValue = new DateTime();
+            dateValue = DateTime.Now;
+
+            Console.WriteLine("The current date and time is {0}.", dateValue);
+            Console.WriteLine("Please enter a number:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, the date and time will be {1}.", x, dateValue.AddHours(x));
+
+            Console.ReadLine();
+        }
+    }
+}
