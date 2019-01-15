ee1c6c42a8b2d74d4b53d0c4cefa0fe41c64d141
Savannah Fox <savannahfox@gmail.com>
2019-01-15 14:29:08 -0800

    Commit changes to game; create and log player ID with GUID.

--- ClassesandObjects/ClassesandObjects/Program.cs
+++ ClassesandObjects/ClassesandObjects/Program.cs
@@ -13,7 +13,9 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name:");
+            const string casinoName = "Grand Hotel and Casino";
+            
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name:", casinoName);
             string playerName = Console.ReadLine();
 
             Console.WriteLine("And how much money did you bring today:");
@@ -25,6 +27,11 @@ namespace TwentyOne
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\My DELL\logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
