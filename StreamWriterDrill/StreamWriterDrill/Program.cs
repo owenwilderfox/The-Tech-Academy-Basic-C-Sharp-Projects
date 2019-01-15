4c6ef542b2ad9b24f59ea02cb17be06a3adf3682
Savannah Fox <savannahfox@gmail.com>
2019-01-15 12:33:50 -0800

    Initial commit of drill.

---
+++ StreamWriterDrill/StreamWriterDrill/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamWriterDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good afternoon! Please enter a number:");
+            int enteredText = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\My DELL\logs\log.txt", true))
+            {
+                file.WriteLine(enteredText);
+            }
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\My DELL\logs\log.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
