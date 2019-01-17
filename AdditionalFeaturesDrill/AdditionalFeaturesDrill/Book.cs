22dbd97db23cce9281d1bf8b152816fc60ad37bb
Savannah Fox <savannahfox@gmail.com>
2019-01-17 09:37:20 -0800

    Commit changes to drill.

---
+++ AdditionalFeaturesDrill/AdditionalFeaturesDrill/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesDrill
+{
+    class Book
+    {
+        public Book(string title) : this(title, 10)
+        {
+        }
+        public Book(string title, int bookQuantity)
+        {
+            string booktitle = title;
+            int BookCount = bookQuantity;
+        }
+        
+        public string booktitle { get; set; }
+        public int BookCount { get; set; }
+
+    }
+}
---
+++ AdditionalFeaturesDrill/AdditionalFeaturesDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string libraryBranch = "Huron St";
+
+            Console.WriteLine("Welcome to the {0} Branch Library.", libraryBranch);
+
+            Book Book = new Book("Emma");
+
+            Console.ReadLine();
+
+        }
+    }
+}
