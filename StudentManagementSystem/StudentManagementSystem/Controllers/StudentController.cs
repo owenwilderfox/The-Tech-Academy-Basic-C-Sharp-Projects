5ad7ddd1f7821188fb908dfa7abb42de1f21df9d
Savannah Fox <savannahfox@gmail.com>
2019-01-21 11:42:20 -0800

    Initial commit of Student Management System project. Add Student controller and model.

---
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString = @"Data Source=MYDELL-PC\SQLEXPRESS;Initial Catalog=School;
+                                            Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                            TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                            MultiSubnetFailover=False";
+        // GET: Student
+        public ActionResult Index()
+        {
+            string queryString = "Select * From Students";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                    students.Add(student);
+                }
+                connection.Close();
+            }
+                return View(students);
+        }
+    }
+}
