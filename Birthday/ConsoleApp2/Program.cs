5128b9c2460aac89a3e9e1053102dc31f994b6f4
Savannah Fox <savannahfox@gmail.com>
2019-01-23 11:09:03 -0800

    Move Birthday app

---
+++ Birthday/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your date of birth (MM/DD/YYYY):");
+            DateTime DOB = DateTime.Parse(Console.ReadLine());
+            TimeSpan age = DateTime.Now - DOB;
+
+            Console.WriteLine("Your age: {0} years", (int)(age.Days / 365.25));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
